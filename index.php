aac2ea44b40fe69be225968f9a5a833bd04984d6
Driglou <driglou@gmail.com>
2015-03-26 22:29:08 +0200

    Updating for fb

--- index.php
+++ index.php
@@ -8,44 +8,22 @@
   <body>
   <script>
   window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '430868697071448',
-      xfbml      : true,
-      version    : 'v2.2'
-    });
+     FB.init({
+       appId      : '430868697071448',
+       xfbml      : true,
+       version    : 'v2.3'
+     });
 
-    // ADD ADDITIONAL FACEBOOK CODE HERE
-  };
+     // ADD ADDITIONAL FACEBOOK CODE HERE
+   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-   function onLogin(response) {
-  if (response.status == 'connected') {
-    FB.api('/me?fields=first_name', function(data) {
-      var welcomeBlock = document.getElementById('fb-welcome');
-      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-    });
-  }
-}
-
-FB.getLoginStatus(function(response) {
-  // Check login status on load, and if the user is
-  // already logged in, go directly to the welcome message.
-  if (response.status == 'connected') {
-    onLogin(response);
-  } else {
-    // Otherwise, show Login dialog first.
-    FB.login(function(response) {
-      onLogin(response);
-    }, {scope: 'user_friends, email'});
-  }
-});
+   (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   </script>
     <h1 id="fb-welcome"></h1>
     <div class = "gallery">
@@ -123,6 +101,29 @@ FB.getLoginStatus(function(response) {
           reverse: false
         });
       });
+    </script>
+    <script>
+    function onLogin(response) {
+      if (response.status == 'connected') {
+        FB.api('/me?fields=first_name', function(data) {
+          var welcomeBlock = document.getElementById('fb-welcome');
+          welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+        });
+      }
+      }
+
+      FB.getLoginStatus(function(response) {
+      // Check login status on load, and if the user is
+      // already logged in, go directly to the welcome message.
+      if (response.status == 'connected') {
+        onLogin(response);
+      } else {
+        // Otherwise, show Login dialog first.
+        FB.login(function(response) {
+          onLogin(response);
+        }, {scope: 'user_friends, email'});
+      }
+    });
     </script>
   </body>
 </html>
