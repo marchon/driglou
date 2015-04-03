abf2f3f7eb7f97b34a48168fcf4ceb84e89a63fe
Driglou <driglou@gmail.com>
2015-04-03 17:06:23 +0300

    S3 BUCKET

--- artworks.php
+++ artworks.php
@@ -2,7 +2,7 @@
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 $s3 = Aws\S3\S3Client::factory();
-$bucket = 'driglou';
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
 <html>
   <head>
